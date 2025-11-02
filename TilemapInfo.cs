42194315bfad9cc98e7cffe12deb1c664059f103
Yvens Rebouças Serpa <y.reboucasserpa@saxion.nl>
2025-11-02 12:15:33 +0100

    Update weaksingleton and tilemapinfo

--- TilemapInfo.cs
+++ TilemapInfo.cs
@@ -6,23 +6,57 @@ namespace UUtils
 {
     public class TilemapInfo : MonoBehaviour
     {
-    
-        [SerializeField]
-        private TilemapRenderer tileMapRenderer;
+        [SerializeField] private TilemapRenderer tileMapRenderer;
+        [SerializeField] private bool getInfoOnAwake = false;
+        [SerializeField] private Vector2Int dimensions;
+        private Bounds _tileMapBounds;
+        private bool _initialized = false;
+
+        private void Awake()
+        {
+            if (getInfoOnAwake)
+            {
+                GetInfo();
+            }
+        }
 
         [Button("Get info")]
         private void GetInfo()
         {
-            var tileMapBounds = tileMapRenderer.bounds;
+            _tileMapBounds = tileMapRenderer.bounds;
 
             // Access the dimensions of the bounds
-            var width = tileMapBounds.size.x;
-            var height = tileMapBounds.size.y;
+            var width = _tileMapBounds.size.x;
+            var height = _tileMapBounds.size.y;
+
+            // Save the dimensions
+            dimensions = new Vector2Int((int)width, (int)height);
+            _initialized = true;
 
             // Output the bounds and dimensions
-            DebugUtils.DebugLogMsg("Tile Map Bounds: " + tileMapBounds);
+            DebugUtils.DebugLogMsg("Tile Map Bounds: " + _tileMapBounds);
             DebugUtils.DebugLogMsg("Width: " + width);
             DebugUtils.DebugLogMsg("Height: " + height);
+        }
+
+        public Bounds GetTileMapBounds()
+        {
+            if (!_initialized)
+            {
+                GetInfo();
+            }
+
+            return _tileMapBounds;
+        }
+
+        public Vector2Int GetDimensions()
+        {
+            if (!_initialized)
+            {
+                GetInfo();
+            }
+
+            return dimensions;
         }
     }
 }
--- WeakSingleton.cs
+++ WeakSingleton.cs
@@ -5,6 +5,7 @@ namespace UUtils
     public class WeakSingleton<T> : MonoBehaviour where T : MonoBehaviour
     {
         #region Singleton
+
         private static T _singleton;
         public static T GetSingleton() => _singleton;
         private static GameObject _singletonObject;
@@ -16,13 +17,15 @@ namespace UUtils
                 //Destroy the current singleton, so the new one takes its place
                 Destroy(_singletonObject);
             }
+
             //Set the singleton to be this object
             _singleton = GetComponent<T>();
             _singletonObject = gameObject;
         }
+
         #endregion
 
-        private void Awake()
+        protected virtual void Awake()
         {
             ControlSingleton();
         }
